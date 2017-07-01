3687235fd913f0fbd6967c0a27a072ca77675a05
Jacek Gratka <arthas29@gmail.com>
2017-07-01 14:32:20 +0200

    Add docker support

---
+++ BuildDockerImages.sh
@@ -0,0 +1,2 @@
+docker build -t shather/fibon-api ./Fibon.api
+docker build -t shather/fibon-service ./Fibon.service
---
+++ dotnetPublish.sh
@@ -0,0 +1,2 @@
+dotnet publish ./Fibon.api -c Release -o ./Bin/Docker
+dotnet publish ./Fibon.service -c Release -o ./Bin/Docker
